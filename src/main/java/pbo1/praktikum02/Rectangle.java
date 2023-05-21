117f3a7a9e5fb117b0fe3c83ac6edaadd4e4beac
iinretno <133147886+iinretno@users.noreply.github.com>
2023-05-21 18:08:27 +0800

    Constructor - sama dengan nama Class

--- src/main/java/pbo1/praktikum02/Rectangle.java
+++ src/main/java/pbo1/praktikum02/Rectangle.java
@@ -2,6 +2,11 @@ package pbo1.praktikum02;
 public class Rectangle {
     double panjang;
     double lebar;
+
+    public Rectangle() {
+        panjang=5;
+        lebar=2;
+    }
     
     void cetakInfo(){
         System.out.println("==================");
