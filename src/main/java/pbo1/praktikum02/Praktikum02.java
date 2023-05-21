1afd9a5d3358dcfae8e5773ee345d0f1b97cde23
iinretno <133147886+iinretno@users.noreply.github.com>
2023-05-21 17:35:12 +0800

    Initial commit

---
+++ src/main/java/pbo1/praktikum02/Praktikum02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pbo1.praktikum02;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class Praktikum02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
